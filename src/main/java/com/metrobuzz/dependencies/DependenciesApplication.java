d5f10570ad35b7dfe09a88e6da952afc88b57070
tylerdgenius <albertnwachukwu@gmail.com>
2024-09-17 13:22:01 +0100

    feat: added profiles to app

--- src/main/java/com/metrobuzz/dependencies/DependenciesApplication.java
+++ src/main/java/com/metrobuzz/dependencies/DependenciesApplication.java
@@ -3,14 +3,10 @@ package com.metrobuzz.dependencies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class DependenciesApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("DATABASE_URI", dotenv.get("DATABASE_URI"));
 		SpringApplication.run(DependenciesApplication.class, args);
 	}
 }
